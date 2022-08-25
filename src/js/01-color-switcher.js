769b3528289d1591cc5d6dcf34c2867de6f2226c
Kateryna Kleshch <ketrins1993@gmail.com>
2022-08-25 19:22:57 +0300

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+stopBtn.disabled = true;
+
+let timerId = null;
+
+const onStartBtnClick = () => {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    document.body.style.backgroundColor = getRandomHexColor();
+
+    timerId = setInterval(() => {
+        const newColor = getRandomHexColor();
+        console.log(newColor);
+        document.body.style.backgroundColor = newColor;
+    }, 1000);
+}
+
+const onStopBtnClick = () => {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+    clearInterval(timerId);
+};
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
