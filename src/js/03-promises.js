c6538652482d6496abe3c089428bb812498ec6e4
Kateryna Kleshch <ketrins1993@gmail.com>
2022-08-27 00:03:38 +0300

    start task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,15 @@
+const promisesInfoFormEl = document.querySelector('.form');
+const createPromisesBtnEl = document.querySelector('button[type="submit"]')
+
+promisesInfoFormEl.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log(event.currentTarget.elements);
+  
+  console.log(event.currentTarget.elements.delay.value);
+  console.log(event.currentTarget.elements.step.value);
+  console.log(event.currentTarget.elements.amount.value);
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +18,11 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
